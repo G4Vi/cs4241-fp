517f00009e40f3deed52b9a6cadb7a65b7e7a648
Gavin Hayes <hayesgav@gmail.com>
2016-11-14 10:57:39 -0500

    printing post

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 var http = require('http')
-  , fs   = require('fs')
-  , url  = require('url')
-  , port = 8080
-  , qs   = require('querystring');
+, fs   = require('fs')
+, url  = require('url')
+, port = 8080
+, qs   = require('querystring');
 
 
 //load movies array from another js file
 var movies = require('./movies')
 
 var server = http.createServer (function (req, res) {
-  var uri = url.parse(req.url, true)
+var uri = url.parse(req.url, true)
 
-  switch( uri.pathname ) {
+switch( uri.pathname ) {
     case '/delete':
-      console.log("uri.pathname")   
-      handleDelete(req, res, uri)
-      break
+    console.log("uri.pathname")   
+    handleDelete(req, res, uri)
+    break
     case '/search':
-      handleSearch(res, uri)
-      break    
+    handleSearch(res, uri)
+    break    
     case '/':
-      sendIndex(res)
-      break
+    sendIndex(res)
+    break
     case '/index.html': //incase a webbrowser requests the site by this
-      sendIndex(res)
-      break
+    sendIndex(res)
+    break
     case '/style.css':
-      sendFile(res, 'style.css', 'text/css')
-      break
+    sendFile(res, 'style.css', 'text/css')
+    break
     case '/js/scripts.js':
-      sendFile(res, 'js/scripts.js', 'text/javascript')
-      break
+    sendFile(res, 'js/scripts.js', 'text/javascript')
+    break
     case '/README.md':
-      sendFile(res, 'README.md', 'text/plain')
-      break
+    sendFile(res, 'README.md', 'text/plain')
+    break
     case '/popcorn.png':
-      sendFile(res, 'popcorn.png', 'image/png')
-      break   
+    sendFile(res, 'popcorn.png', 'image/png')
+    break   
     default:
-      res.end('404 not found')
-  }
+    res.end('404 not found')
+}
 
 })
 
@@ -48,13 +48,13 @@ console.log('listening on 8080')
 
 
 function handleDelete(req, res, uri){
-       //http://stackoverflow.com/a/8640308/2405902
-       var contentType = 'text/html'
-       res.writeHead(200, {'Content-type': contentType})
-       
-       console.log(req.method)
-       
-       if (req.method == 'POST') {
+    //http://stackoverflow.com/a/8640308/2405902
+    var contentType = 'text/html'
+    res.writeHead(200, {'Content-type': contentType})
+    
+    console.log(req.method)
+    
+    if (req.method == 'POST') {
         var body = '';
 
         req.on('data', function (data) {
@@ -70,35 +70,38 @@ function handleDelete(req, res, uri){
             
             
             var html = ''
-            html = post['movie']
+            html = escapeRegExp(post['movie'])
             res.end(html)
         });
     }
     else{
-        console.log("not post")
-        res.end('404 not found')
+        console.log("not post")        
     }
- 
+    
+    sendIndex(res)
+    
+    
+
 }
 
 
 function handleSearch(res, uri) {
-  var contentType = 'text/html'
-  res.writeHead(200, {'Content-type': contentType})
+var contentType = 'text/html'
+res.writeHead(200, {'Content-type': contentType})
 
-  var html = ''
-  //if printing the whole page (not ajax) print the items above the movie list
-  if(uri.query && (!uri.query.inline))
-  {
-      html = printHTMLStart()       
-  }
-  
-  //if there is a search query
-  if(uri.query && (uri.query.search !== "")) {   
+var html = ''
+//if printing the whole page (not ajax) print the items above the movie list
+if(uri.query && (!uri.query.inline))
+{
+    html = printHTMLStart()       
+}
+
+//if there is a search query
+if(uri.query && (uri.query.search !== "")) {   
     console.log(uri.query)       
     
     var filteredMovies = [];
-   
+
     //loop through all the movies and try to match substrings
     for(var i = 0; i < movies.length; i++)
     {
@@ -120,102 +123,97 @@ function handleSearch(res, uri) {
         html = html + '<h2>Movies with "' + uri.query.search + '" in the name</h2>' + arrayToHTMLList(filteredMovies)       
     }
     
-     
     
     
-  } else {
-      //on error or blank query print all movies
-      html = html + '<h2>All Movies</h2>' + arrayToHTMLList(movies)      
-  }
-  
-  //add javascript and closing tags if it is not an inline request
-  if(!uri.query.inline)
-  {
-      html = html + printHTMLEnd()        
-  }
-  
-  //finally send it
-  res.end(html)
+    
+} else {
+    //on error or blank query print all movies
+    html = html + '<h2>All Movies</h2>' + arrayToHTMLList(movies)      
 }
 
-//avoid regex crashing the app if a backslash is submitted
-function escapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+//add javascript and closing tags if it is not an inline request
+if(!uri.query.inline)
+{
+    html = html + printHTMLEnd()        
+}
+
+//finally send it
+res.end(html)
 }
 
 function printHTMLStart()
 {
-  var html = ''
-  html = html + '<html>'
+var html = ''
+html = html + '<html>'
 
-  html = html + '<head>'
-  
-  html = html + ' <title>CS4241 Assignment 4 - Gavin Hayes</title> <meta charset="utf-8"> <link rel="stylesheet" type="text/css" href="style.css"/>'
-  html = html + '<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">'
-  
-  html = html + '</head>'
+html = html + '<head>'
 
-  html = html + '<body>'
-  html = html + '<div class="backimg" id="popback">'
-  html = html + '</div>'
-  html = html + '<div class = "content">'
-  html = html + '<h1>Movie Search</h1>'
+html = html + ' <title>CS4241 Assignment 4 - Gavin Hayes</title> <meta charset="utf-8"> <link rel="stylesheet" type="text/css" href="style.css"/>'
+html = html + '<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">'
 
-  html = html + '<form action="search" method="get">'
-  html = html + '<input type="text" name="search" id="searchbox" autocomplete="off"/>'
-  html = html + '<button type="submit">Search</button>'
-  html = html + '</form>'
+html = html + '</head>'
 
-  html = html + '<div id=movielist>'
-  return html
+html = html + '<body>'
+html = html + '<div class="backimg" id="popback">'
+html = html + '</div>'
+html = html + '<div class = "content">'
+html = html + '<h1>Movie Search</h1>'
+
+html = html + '<form action="search" method="get">'
+html = html + '<input type="text" name="search" id="searchbox" autocomplete="off"/>'
+html = html + '<button type="submit">Search</button>'
+html = html + '</form>'
+
+html = html + '<div id=movielist>'
+return html
 }
 
 function printHTMLEnd()
 {
-  var html = '</div></div>'    
+var html = '</div></div>'    
     
-  html = html + '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>'
-  html = html + '<script src="js/scripts.js"></script>'
-  
-  html = html + '</body>'
-  html = html + '</html>'
-  
-  return html
+html = html + '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>'
+html = html + '<script src="js/scripts.js"></script>'
+
+html = html + '</body>'
+html = html + '</html>'
+
+return html
     
 }
 
 // Note: consider this your "index.html" for this assignment
 function sendIndex(res) {
-  var contentType = 'text/html'
-  
-  var html = printHTMLStart()
+var contentType = 'text/html'
 
-  html = html + '<h2>All Movies</h2>'
-  html = html + arrayToHTMLList(movies) 
-  
-  html = html + printHTMLEnd()
-  
-  
-  res.writeHead(200, {'Content-type': contentType})
-  res.end(html, 'utf-8')
+var html = printHTMLStart()
+
+html = html + '<h2>All Movies</h2>'
+html = html + arrayToHTMLList(movies) 
+
+html = html + printHTMLEnd()
+
+
+res.writeHead(200, {'Content-type': contentType})
+res.end(html, 'utf-8')
 }
 
 function sendFile(res, filename, contentType) {
-  contentType = contentType || 'text/html'
+contentType = contentType || 'text/html'
 
-  fs.readFile(filename, function(error, content) {
+fs.readFile(filename, function(error, content) {
     res.writeHead(200, {'Content-type': contentType})
     res.end(content, 'utf-8')
-  })
+})
 
 }
 
 function arrayToHTMLList(movieNames)
 {  
-   /* var html = '<ul>'
+/* var html = '<ul>'
     html = html + movieNames.map(createListItem).join(' ')    
     html = html + '</ul>'*/
-   
+
     var html = '<table>'
     html = html + '<thead><tr><th>Movie</th><th>Delete</th></tr</thead>'
     html = html + '<tbody>'
@@ -257,4 +255,9 @@ function createListItem(d)
     html = html + '</form>' 
     
     return html
+}
+
+//avoid regex crashing the app if a backslash is submitted
+function escapeRegExp(str) {
+return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
