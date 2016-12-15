f05a7467ffb42aac9de8495238b31b9182e0e30b
nchaput <nchaput@wpi.edu>
2016-12-14 19:13:16 -0500

    chart now in descending order and is sized by percentage not fixed pixel values

--- js/datavis.js
+++ js/datavis.js
@@ -14,12 +14,13 @@ var DV; //global namespace for Data Visualization constants and functions
                 data.push({tag: pair[0], count: parseInt(pair[1],10)});
             });
 
-            return data;
+            //sort the list in descending order by number of appearences
+            return data.sort(function(a, b){return b.count - a.count});
         },
 
         createChart: function () {
-            var width = 420,
-                barHeight = 20;
+            var width = 600,
+                barHeight = 40;
 
             /*var data = [
                 {tag: 'p', count: 4},
@@ -30,15 +31,21 @@ var DV; //global namespace for Data Visualization constants and functions
                 {tag: 'head', count: 42}
             ];*/
 
+            //get data and clear the chart div
             var data = DV.getChartData();
             document.getElementById("chart").innerHTML = '';
 
+            //create the chart
+
             var svg = d3.select('.chart')
-                .attr('width', width) // set its dimentions
-                .attr('height', (barHeight + 5) * data.length)
+                .append("div")
+                .classed("svg-container", true) //container class to make it responsive
                 .append('svg')        // create an <svg> element
-                .attr('width', width) // set its dimentions
-                .attr('height', (barHeight + 5) * data.length);
+                //responsive SVG needs these 2 attributes and no width and height attr
+                .attr("preserveAspectRatio", "xMinYMin meet")
+                .attr("viewBox","0 0 " + width + " " + (barHeight+2)*data.length)
+                //class to make it responsive
+                .classed("svg-content-responsive", true);
 
             var x = d3.scaleLinear()
                 .range([0, width])
